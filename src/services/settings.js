f4d0435af65fe5ebfdde9f187a2bdbc42b5c2694
Sergio del Pino C <sdelpinocampos@hotmail.com>
2024-07-01 18:51:58 -0400

    Fix vite config

--- src/services/settings.js
+++ src/services/settings.js
@@ -1,8 +1,8 @@
-let API_KEY = import.meta.env.API_KEY;
+let API_KEY = import.meta.env.API_KEY_;
 
-let API_URL = import.meta.env.API_URL;
+let API_URL = import.meta.env.API_URL_;
 
-let ENDPOINT = import.meta.env.ENDPOINT;
+let ENDPOINT = import.meta.env.ENDPOINT_;
 
 if (import.meta.env.MODE === 'development') {
   API_KEY = import.meta.env.VITE_API_KEY
--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   define: {
-    "process.env": process.env,
-    API_KEY: process.env.API_KEY,
-    API_URL: process.env.API_URL,
-    ENDPOINT: process.env.ENDPOINT
+    API_KEY_: process.env.VITE_API_KEY,
+    API_URL_: process.env.VITE_API_URL,
+    ENDPOINT_: process.env.VITE_ENDPOINT
   }
 })
