bcd8d36645e29fd795e3c42db9c68b202dd4d255
Sergio del Pino C <sdelpinocampos@hotmail.com>
2024-07-01 18:14:11 -0400

    Change settings for production

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 1. Clone the repository
 2. Run `yarn`
 3. Run `yarn dev`
+4. For production add the .env variables API_KEY, API_URL and ENDPOINT
 
 ## Rest-Server
 
--- src/services/settings.js
+++ src/services/settings.js
@@ -1,13 +1,17 @@
+let API_KEY = import.meta.env.API_KEY;
 
-// console.log('import.meta.env.MODE: ', import.meta.env.MODE)
-export const API_KEY = import.meta.env.VITE_API_KEY;
-
-export const API_URL = import.meta.env.VITE_API_URL;
+let API_URL = import.meta.env.API_URL;
 
 let ENDPOINT = import.meta.env.ENDPOINT;
 
 if (import.meta.env.MODE === 'development') {
+  API_URL = import.meta.env.VITE_API_URL
+  API_KEY = import.meta.env.VITE_API_KEY
   ENDPOINT = import.meta.env.VITE_ENDPOINT
 }
 
-export { ENDPOINT }
+export {
+  API_URL,
+  API_KEY,
+  ENDPOINT
+}
