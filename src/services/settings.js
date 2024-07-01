d5c7d93349414b20f47ff23d00cc423462ba7bbe
Sergio del Pino C <sdelpinocampos@hotmail.com>
2024-07-01 18:29:00 -0400

    Define environment variables

--- src/services/settings.js
+++ src/services/settings.js
@@ -5,13 +5,13 @@ let API_URL = import.meta.env.API_URL;
 let ENDPOINT = import.meta.env.ENDPOINT;
 
 if (import.meta.env.MODE === 'development') {
-  API_URL = import.meta.env.VITE_API_URL
   API_KEY = import.meta.env.VITE_API_KEY
+  API_URL = import.meta.env.VITE_API_URL
   ENDPOINT = import.meta.env.VITE_ENDPOINT
 }
 
 export {
-  API_URL,
   API_KEY,
+  API_URL,
   ENDPOINT
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+    API_KEY: process.env.API_KEY,
+    API_URL: process.env.API_URL,
+    ENDPOINT: process.env.ENDPOINT
+  }
 })
